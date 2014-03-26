a1e78043dd4809a9dc8b9d0842344565399f3b67
Denis Pismenny <denis.pismenny@gmail.com>
2014-03-26 23:39:08 +0200

    add fixed navbar

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 
+	function fixedSidenav() {
+		var top = ($('.page').offset().top - 20);
+		var bottom = ($('.page').offset().top - 20);
+
+		if($(window).scrollTop() > top){
+				$(".js-navbar").addClass('is-fixed');
+		}
+		if($(window).scrollTop() < bottom){
+			$(".js-navbar").removeClass('is-fixed');
+		}
+	}
+
+	$(window).scroll(function() {
+			if ($('.js-navbar').length) {
+				fixedSidenav();
+			}
+		});
+
 });
