1a2b9f607b9f1e60be2921f17e2927b9546237bf
Denis Pismenny <denis.pismenny@gmail.com>
2014-03-27 12:31:14 +0200

    add font awesome

--- js/common.js
+++ js/common.js
@@ -1,21 +1,38 @@
 $(document).ready(function() {
 
-	function fixedSidenav() {
-		var top = ($('.page').offset().top - 20);
-		var bottom = ($('.page').offset().top - 20);
+	function fixNavbar() {
+		var el = $('.js-navbar'),
+				el_top = ($('.page').offset().top),
+				el_bottom = ($('.page').offset().top);
 
-		if($(window).scrollTop() > top){
-				$(".js-navbar").addClass('is-fixed');
+		if($(window).scrollTop() > el_top){
+				el.addClass('is-fixed');
 		}
-		if($(window).scrollTop() < bottom){
-			$(".js-navbar").removeClass('is-fixed');
+		if($(window).scrollTop() < el_bottom){
+			el.removeClass('is-fixed');
 		}
 	}
 
-	$(window).scroll(function() {
-			if ($('.js-navbar').length) {
-				fixedSidenav();
+	function searchSuggest() {
+		var el = $('.js-searchsuggest'),
+				el_drop = ('.search__dropdown');
+
+		el.on('click', function() {
+			$(this).find(el_drop).toggleClass('is-open').slideToggle('fast');
+		});
+
+		//click document
+		$(document).click(function(event) {
+			if($(event.target).parents().index( el ) == -1) {
+				el.find( el_drop ).removeClass('is-open').slideUp('fast');
 			}
 		});
+	} searchSuggest();
+
+	$(window).scroll(function() {
+		if ($('.js-navbar').length) {
+			fixNavbar();
+		}
+	});
 
 });
