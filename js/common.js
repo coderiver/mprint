f7cd9589e454e6beddf9f44a7c8f3a29ee9ccd87
Denis Pismenny <denis.pismenny@gmail.com>
2014-03-28 16:40:29 +0200

    add main services

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	function parallax() {
 		var scrolled = $(window).scrollTop();
-		$('#parallax-bg').css('top',-(scrolled*0.02)+'px');
+		$('#parallax-bg').css('top',-(scrolled*0.08)+'px');
 	}
 
 	function fixNavbar() {
