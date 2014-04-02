eecdb79c9f88500065c6757a9dd3fff3c2cd676d
Denis Pismenny <denis.pismenny@gmail.com>
2014-04-02 17:48:25 +0300

    add faq.html

--- js/common.js
+++ js/common.js
@@ -18,6 +18,36 @@ $(document).ready(function() {
 		}
 	}
 
+	function posSidebar() {
+
+		var el = $('.js-sidebar'),
+				el_top = ($('.page').offset().top),
+				el_bottom = ($('.page').offset().top),
+				// top = $('.js-navbar').outerHeight(),
+				rt = ($(window).width() - ($('.page').offset().left + $('.page').outerWidth()));
+				el_height = $('.js-sidebar').outerHeight();
+
+		$('.has-fixedsidebar .l-main').css("min-height", el_height);
+
+		if($(window).scrollTop() > el_top){
+				el.addClass('is-fixed');
+				el.css({"right": rt});
+		}
+		if($(window).scrollTop() < el_bottom){
+			el.removeClass('is-fixed');
+			el.css({"right": "auto"});
+		}
+	} posSidebar();
+
+	function sideAccord() {
+		$('.js-sidenav .sidenav__head').on('click', function() {
+			// alert('gogogo');
+			$('.js-sidenav').children('li').removeClass('is-active');
+			$(this).parent().addClass('is-active');
+		});
+		// return false;
+	} sideAccord();
+
 	function searchSuggest() {
 		var el = $('.js-searchsuggest'),
 				el_drop = ('.search__dropdown');
@@ -39,7 +69,11 @@ $(document).ready(function() {
 			fixNavbar();
 		}
 		parallax();
+		posSidebar();
 
+	});
+	$(window).resize(function() {
+		posSidebar();
 	});
 
 	// skrollr.init({
